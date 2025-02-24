afd7a41da253c0dc35be4cc81f11ece7709becf6
wallace ribeiro <wallaceribeiro72@gmail.com>
2025-02-24 12:31:39 -0300

    documentation about js file

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const inputText = document.getElementById("input");
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 
+//função para criar o item da lista
 const createItem = (value) => {
+
+    //criando a div e colocando a classe item nela
     const divItem = document.createElement("div");
     divItem.classList.add("item");
 
+    //estrutura do item com a inserção do valor dinamico
     divItem.innerHTML = `
         <div>
             <img src="assets/icons/bullet.svg" alt="">
@@ -25,11 +29,16 @@ const createItem = (value) => {
 // Evento para adicionar um item na lista
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+
     const value = inputText.value.trim();
 
     if (value === "") return; // Evita adicionar itens vazios
 
+    //utiliza a função de criar o item passando como parametro o valor dinamico
     const newItem = createItem(value);
+
+    //appendChild para adicionar como filho
     ul.appendChild(newItem);
+    
     inputText.value = ""; // Limpa o input após adicionar
 });
